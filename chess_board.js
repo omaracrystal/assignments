9841e5a730e3cbac3e3f6538dc9dded8fa093050
omaracrystal <omaracrystal@gmail.com>
2015-07-12 21:56:20 -0600

    8x8 chessboard

---
+++ chess_board.js
@@ -0,0 +1,13 @@
+//var userInput = prompt("How wide do you want your chessboard to be?")
+
+var string1 = " #"
+var string2 = "# "
+var newline = "\n"
+var counter = .5;
+
+var chessboard = string1 + newline + string2
+
+while (counter <= 3.5) {
+  console.log(chessboard);
+  counter++
+ }
---
+++ chess_board2.js
@@ -0,0 +1,10 @@
+var string1 = " # # # # # # # #";
+var string2 = "# # # # # # # ";
+var newline = "\n";
+var counter = 0.5;
+
+while (counter <= 4){
+  console.log(string1 + newline);
+  console.log(string2 + newline);
+  counter++
+}
