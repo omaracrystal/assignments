6ad5fb8cbfa0e9e2d043eead0977544395220dea
omaracrystal <omaracrystal@gmail.com>
2015-07-13 00:25:57 -0600

    Added comments and took out var newline= n

--- chess_board.js
+++ chess_board.js
@@ -3,12 +3,14 @@ var userInput = prompt("How wide do you want your chessboard to be?")
 //converts userInput from string to number
 var userInput = Number(userInput)
 
-//uses
+//.join repeats the string by the number input by the user
 var string1 = Array(userInput + 1).join(" #")
 var string2 = Array(userInput + 1).join("# ")
-var newline = "\n"
 
-//
+//the ? and : is called ternary operator
+// ternary operator takes in three arguments
+// condition ? exprr1 : expr2
+//short cut for if statement
 var chessboard = function (i) {
   console.log(i % 2 == 0 ? string1 : string2);
 };
