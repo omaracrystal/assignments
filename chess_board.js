48f3dbfcd62c274c2ea5c6c9567409e0919ede3f
omaracrystal <omaracrystal@gmail.com>
2015-07-12 23:49:45 -0600

    Fixed error with string output to match userInput by adding 1 to the userInput

--- chess_board.js
+++ chess_board.js
@@ -1,14 +1,18 @@
 var userInput = prompt("How wide do you want your chessboard to be?")
 
+//converts userInput from string to number
 var userInput = Number(userInput)
 
-var string1 = Array(userInput).join(" #")
-var string2 = Array(userInput).join("# ")
+//uses
+var string1 = Array(userInput + 1).join(" #")
+var string2 = Array(userInput + 1).join("# ")
 var newline = "\n"
 
+//
 var chessboard = function (i) {
   console.log(i % 2 == 0 ? string1 : string2);
 };
+
 
 for (var i = 0; i < userInput; i++) {
   chessboard(i);
