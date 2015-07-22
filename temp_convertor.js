49bee49761e0df63a2e48c6ce45c27d98c72bad0
omaracrystal <omaracrystal@gmail.com>
2015-07-21 18:39:35 -0600

    Created tempurture convertor

---
+++ temp_convertor.js
@@ -0,0 +1,29 @@
+// Write code that asks the user for a temperature in Celsius and converts it to Fahrenheit. Bonus: ask the user first if they want to convert from F to C or C to F.
+
+for (var i=1; i <= 1337; i++) {
+  if ((i % 2) == 1) {
+  console.log(i);
+ }
+}
+
+
+var userInput = prompt("Please either enter if you would like to convert Fahrenheit to Celsius by typing F to C or or if you would like to convert Celsius to Fahrenheit by typing C to F");
+
+if (userInput == "F to C" || "f to c") {
+
+	alert();
+} else if (userInput == "C to F" || "c to f") {
+	alert(false);
+} else {
+  alert("Error: please type C to F or F to C");
+}
+
+
+
+
+for (userInput= 0; userInput<= 36; i++) {
+  if ("userInput" === "spinner") {
+    alert("Win!");
+  } else {
+    prompt("Try Again.");
+  }
