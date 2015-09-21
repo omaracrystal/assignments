686590407ddf1684af269693f165bb5056b93feb
omaracrystal <omaracrystal@gmail.com>
2015-09-21 15:43:07 -0600

    added recursive function for fizzbuzz

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -8,3 +8,33 @@ for (var i=1; i<=100; i++) {
   else
     console.log(i);
 }
+
+
+// function fizzBuzz (num) {
+//   if (num === 0) {
+//     return;
+//   } else if (num % 15 === 0) {
+//     console.log('FizzBuzz');
+//     fizzBuzz(num -1);
+//   } else if (num % 3 === 0) {
+//     console.log("Buzz");
+//     fizzBuzz(num -1);
+//   } else if (num % 5 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(num);
+//     fizzBuzz(num -1);
+//   }
+// }
+
+function fizzbuzz(num) {
+  if (num === 0) {
+    return -1
+  }
+  var output = "";
+    if(num % 5 === 0) output += 'fizz';
+    if(num % 3 === 0) output += 'buzz';
+
+  console.log(output || num);
+  fizzbuzz(num -1);
+}
